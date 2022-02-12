03421d682c9a7032724d2360743765c037f72537
Marko Manninen <elonmedia@gmail.com>
2022-02-13 01:04:26 +0200

    prepare for local pip install
    enabling calling tpe from jupyter notebook

--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
 #
 # Copyright (C) 2021-2022 Marko Manninen
 #
+# pip install -e .
+
 from distutils.core import setup
 
 setup(
-    name="Tandem Piercer Experiment",
-    version="1.0",
-    description="Tandem Piercer Experiment",
-    author="Marko Manninen",
-    author_email="elonmedia@gmail.com",
-    url="https://www.mesokosmos.com",
-    packages=["tpe"]
+    name = "Tandem Piercer Experiment",
+    version = "0.0.1",
+    description = "Tandem Piercer Experiment",
+    author = "Marko Manninen",
+    author_email = "elonmedia@gmail.com",
+    url = "https://www.mesokosmos.com",
+    packages = ["tpe"]
 )
