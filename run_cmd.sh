46fc95a6b1f2b3d37e38bb8515d7f746b8a17892
Marko Manninen <elonmedia@gmail.com>
2022-02-13 03:48:47 +0200

    long run commands

--- run_cmd.sh
+++ run_cmd.sh
@@ -15,6 +15,9 @@ python run.py --pulse_source="Background" --store_statistics=2 --pulse_detection
 
 python run.py --pulse_source="Background" --store_statistics=2 --pulse_detection_mode=0 --simple_trigger=1 --timebase=63 --pre_trigger_samples=5000 --post_trigger_samples=5000 --execution_time=1400 --simple_trigger_alternate=1
 
-python run.py --pulse_source="Background" --store_statistics=2 --pulse_detection_mode=0 --simple_trigger=0 --timebase=2 --pre_trigger_samples=5000 --post_trigger_samples=5000 --execution_time=1400
 
-python run.py --pulse_source="Background" --store_statistics=2 --pulse_detection_mode=0 --simple_trigger=1 --timebase=2 --pre_trigger_samples=5000 --post_trigger_samples=5000 --execution_time=1400 --simple_trigger_alternate=1
+python run.py --pulse_source="Background" --store_statistics=2 --pulse_detection_mode=1 --simple_trigger=0 --timebase=63 --pre_trigger_samples=5000 --post_trigger_samples=5000 --execution_time=21600 --experiment_name="Detectors on top of each other - No trigger long run"
+
+python run.py --pulse_source="Background" --store_statistics=2 --pulse_detection_mode=1 --simple_trigger=1 --timebase=63 --pre_trigger_samples=5000 --post_trigger_samples=5000 --execution_time=21600 --experiment_name="Detectors on top of each other - Trigger long run" --simple_trigger_alternate=0
+
+python run.py --pulse_source="Background" --store_statistics=2 --pulse_detection_mode=1 --simple_trigger=1 --timebase=63 --pre_trigger_samples=5000 --post_trigger_samples=5000 --execution_time=21600 --experiment_name="Detectors on top of each other - Alternating trigger long run" --simple_trigger_alternate=1
