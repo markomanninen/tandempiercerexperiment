65930bf9f04161e7541aac705c30e71bb053d4b5
Marko Manninen <elonmedia@gmail.com>
2022-02-15 07:45:04 +0200

    new commands

--- run_cmd.sh
+++ run_cmd.sh
@@ -10,20 +10,32 @@ python run.py --pulse_source="Cd-109 1mci" --headless_mode=1 --execution_time=20
 python run.py --pulse_source="Cd-109 1mci" --store_waveforms=1 --store_waveforms_channels=CD
 
 
+#
 
 python run.py --pulse_source="Background" --store_statistics=2 --pulse_detection_mode=0 --simple_trigger=0 --timebase=63 --pre_trigger_samples=5000 --post_trigger_samples=5000 --execution_time=1400
 
 python run.py --pulse_source="Background" --store_statistics=2 --pulse_detection_mode=0 --simple_trigger=1 --timebase=63 --pre_trigger_samples=5000 --post_trigger_samples=5000 --execution_time=1400 --simple_trigger_alternate=1
 
 
+# Detectors apart from each other - long run
 
 python run.py --pulse_source="Background" --store_statistics=2 --pulse_detection_mode=1 --simple_trigger=0 --timebase=63 --pre_trigger_samples=5000 --post_trigger_samples=5000 --execution_time=21600 --experiment_name="Detectors apart from each other - No trigger long run"
 
 python run.py --pulse_source="Background" --store_statistics=2 --pulse_detection_mode=1 --simple_trigger=1 --timebase=63 --pre_trigger_samples=5000 --post_trigger_samples=5000 --execution_time=21600 --experiment_name="Detectors apart from each other - Alternating trigger long run" --simple_trigger_alternate=1
 
 
-python run.py --pulse_source="Background" --store_statistics=2 --pulse_detection_mode=1 --simple_trigger=1 --timebase=2 --pre_trigger_samples=5000 --post_trigger_samples=5000 --execution_time=10800 --experiment_name="Detectors apart from each other - Alternating trigger long run" --simple_trigger_alternate=1
+# Alternating trigger 2ns
 
-python run.py --pulse_source="Background" --store_statistics=2 --pulse_detection_mode=1 --simple_trigger=1 --timebase=2 --pre_trigger_samples=5000 --post_trigger_samples=5000 --execution_time=10800 --experiment_name="Detectors next to each other - Alternating trigger long run" --simple_trigger_alternate=1
+python run.py --pulse_source="Background" --store_statistics=2 --pulse_detection_mode=1 --simple_trigger=1 --timebase=2 --pre_trigger_samples=5000 --post_trigger_samples=5000 --execution_time=10800 --experiment_name="Detectors apart from each other - Alternating trigger 2ns" --simple_trigger_alternate=1
 
-python run.py --pulse_source="Background" --store_statistics=2 --pulse_detection_mode=1 --simple_trigger=1 --timebase=2 --pre_trigger_samples=5000 --post_trigger_samples=5000 --execution_time=10800 --experiment_name="Detectors on top of each other - Alternating trigger long run" --simple_trigger_alternate=1
+python run.py --pulse_source="Background" --store_statistics=2 --pulse_detection_mode=0 --simple_trigger=1 --timebase=2 --pre_trigger_samples=5000 --post_trigger_samples=5000 --execution_time=10800 --experiment_name="Detectors apart from each other - Alternating trigger 2ns" --simple_trigger_alternate=1
+
+
+python run.py --pulse_source="Background" --store_statistics=2 --pulse_detection_mode=1 --simple_trigger=1 --timebase=2 --pre_trigger_samples=5000 --post_trigger_samples=5000 --execution_time=10800 --experiment_name="Detectors next to each other - Alternating trigger 2ns" --simple_trigger_alternate=1
+
+python run.py --pulse_source="Background" --store_statistics=2 --pulse_detection_mode=0 --simple_trigger=1 --timebase=2 --pre_trigger_samples=5000 --post_trigger_samples=5000 --execution_time=10800 --experiment_name="Detectors next to each other - Alternating trigger 2ns" --simple_trigger_alternate=1
+
+
+python run.py --pulse_source="Background" --store_statistics=2 --pulse_detection_mode=1 --simple_trigger=1 --timebase=2 --pre_trigger_samples=5000 --post_trigger_samples=5000 --execution_time=10800 --experiment_name="Detectors on top of each other - Alternating trigger 2ns" --simple_trigger_alternate=1
+
+python run.py --pulse_source="Background" --store_statistics=2 --pulse_detection_mode=0 --simple_trigger=1 --timebase=2 --pre_trigger_samples=5000 --post_trigger_samples=5000 --execution_time=10800 --experiment_name="Detectors on top of each other - Alternating trigger 2ns" --simple_trigger_alternate=1
