86427eed5156049c8d440479766a2a42c5dc6e04
Marko Manninen <elonmedia@gmail.com>
2022-02-15 21:32:27 +0200

    Detectors next to each other - Alternating trigger 2ns experiments

--- run_cmd.sh
+++ run_cmd.sh
@@ -36,6 +36,6 @@ python run.py --pulse_source="Background" --store_statistics=2 --pulse_detection
 python run.py --pulse_source="Background" --store_statistics=2 --pulse_detection_mode=0 --simple_trigger=1 --timebase=2 --pre_trigger_samples=5000 --post_trigger_samples=5000 --execution_time=10800 --experiment_name="Detectors next to each other - Alternating trigger 2ns" --simple_trigger_alternate=1
 
 
-python run.py --pulse_source="Background" --store_statistics=2 --pulse_detection_mode=1 --simple_trigger=1 --timebase=2 --pre_trigger_samples=5000 --post_trigger_samples=5000 --execution_time=10800 --experiment_name="Detectors on top of each other - Alternating trigger 2ns" --simple_trigger_alternate=1
+python run.py --pulse_source="Background" --store_statistics=2 --pulse_detection_mode=1 --simple_trigger=1 --timebase=2 --pre_trigger_samples=5000 --post_trigger_samples=5000 --execution_time=10800 --experiment_name="Detectors on top of each other - Alternating trigger 2ns" --simple_trigger_alternate=1 --detector_geometry=top
 
-python run.py --pulse_source="Background" --store_statistics=2 --pulse_detection_mode=0 --simple_trigger=1 --timebase=2 --pre_trigger_samples=5000 --post_trigger_samples=5000 --execution_time=10800 --experiment_name="Detectors on top of each other - Alternating trigger 2ns" --simple_trigger_alternate=1
+python run.py --pulse_source="Background" --store_statistics=2 --pulse_detection_mode=0 --simple_trigger=1 --timebase=2 --pre_trigger_samples=5000 --post_trigger_samples=5000 --execution_time=10800 --experiment_name="Detectors on top of each other - Alternating trigger 2ns" --simple_trigger_alternate=1 --detector_geometry=top
