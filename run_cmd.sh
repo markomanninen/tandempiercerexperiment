fe575c13fce76881372e1830922b411ff42b1223
Marko Manninen <elonmedia@gmail.com>
2022-02-24 17:50:59 +0200

    Update run_cmd.sh

--- run_cmd.sh
+++ run_cmd.sh
@@ -42,7 +42,7 @@ python run.py --pulse_source="Background" --store_statistics=2 --pulse_detection
 
 # Whole spectra
 
-python run.py --pulse_source="Background" --store_statistics=2 --pulse_detection_mode=0 --simple_trigger=1 --timebase=2 --pre_trigger_samples=500 --post_trigger_samples=500 --execution_time=600 --experiment_name="Background whole spectrum" --simple_trigger_alternate=1 --detector_geometry=true --sca_module_settings_a=4.0,3.75,10.0,0.70 --sca_module_settings_b=4.0,10.0,10.0,0.4 --high_voltage=-1000 --spectrum_low_limits=4096,4096,842,576 --headless_mode=0
+python run.py --pulse_source="Background" --store_statistics=2 --pulse_detection_mode=0 --simple_trigger=1 --timebase=2 --pre_trigger_samples=500 --post_trigger_samples=500 --execution_time=600 --experiment_name="Background whole spectrum" --simple_trigger_alternate=1 --detector_geometry=next --sca_module_settings_a=4.0,3.75,10.0,0.70 --sca_module_settings_b=4.0,10.0,10.0,0.4 --high_voltage=-1000 --spectrum_low_limits=4096,4096,842,576 --headless_mode=1
 
 python run.py --pulse_source="Cd-109 1μci" --store_statistics=2 --pulse_detection_mode=0 --simple_trigger=1 --timebase=2 --pre_trigger_samples=500 --post_trigger_samples=500 --execution_time=600 --experiment_name="Cd-109 1μci whole spectrum" --simple_trigger_alternate=1 --detector_geometry=true --sca_module_settings_a=4.0,3.75,10.0,0.70 --sca_module_settings_b=4.0,10.0,10.0,0.4 --high_voltage=-1000 --spectrum_low_limits=4096,4096,842,576 --headless_mode=0
 
